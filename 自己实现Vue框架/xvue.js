cbc31473c500f5b24f762314ea280cd3bba3d3dd
WangXinSheng <wangxinsheng@karrytech.com>
2021-07-22 00:02:42 +0800

    自定义Vue

--- 自己实现Vue框架/xvue.js
+++ 自己实现Vue框架/xvue.js
@@ -1,9 +1,60 @@
-let Vue;
+class Vue{
+    constructor(options){
+        this.$options = options;
 
-// Vue.createApp = function(options){
-    
-// }
+        // data代理，使得options中的data可以被直接访问
+        this.proxyData(this,this.$options.data);
 
-Vue.aaaaa = 1231312
+        // 使得整个Vue子实例变为响应式的
+        this.observe(this);
 
-export default Vue;
+    }
+
+    // 观察响应式对象
+    observe(targetObject){
+        if(typeof targetObject === 'object'){
+            Object.keys(targetObject).forEach(key => {
+                this.defineReactive(targetObject,key,targetObject[key])
+            })
+        }
+    }
+
+    // 响应化数据
+    defineReactive(targetObject,key,originValue){
+        if(typeof(originValue) === 'object'){
+            this.observe(originValue)
+        }
+        Object.defineProperty(targetObject,key,{
+            get(){
+                return targetObject[`_${key}`,originValue] || originValue;
+            },
+            set:(newValue) => {
+                if(newValue !== originValue){
+                    targetObject[`_${key}`] = newValue;
+                    if(typeof newValue === 'object'){
+                        observe(newValue);
+                    }
+                    this.updateViews(newValue)
+                }
+            }
+        })
+    }
+
+
+    // 更新视图
+    updateViews(newValue){
+        const targetDom = document.querySelector(this.$options.el);
+        targetDom.innerText = JSON.stringify(newValue); 
+    }
+
+    // 数据代理
+    proxyData(instance,data){
+        Object.keys(data).forEach(key => {
+            Object.defineProperty(instance,key,{
+                get(){
+                    return data[key];
+                }
+            })
+        })
+    }
+}
